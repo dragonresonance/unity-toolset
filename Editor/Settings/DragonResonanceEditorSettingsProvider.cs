ec5f6a29c3d949eeda01403d67778543ce9fcd31
Tabernero <16870364+davidtabernerom@users.noreply.github.com>
2026-04-03 16:27:39 +0200

    Add bug reporting links, decorate the copyright, delete unused directory

--- Editor/Settings/DragonResonanceEditorSettingsProvider.cs
+++ Editor/Settings/DragonResonanceEditorSettingsProvider.cs
@@ -11,7 +11,8 @@ namespace DragonResonance.Editor.Settings
 	{
 		private const string SettingsPath = "Project/Dragon Resonance";
 		private const string BannerGUID = "9a60bf40f7c4aa74d94ea7c32361af13";
-		private const int LargePadding = 36;
+		private const int LargePadding = 60;
+		private const int MediumPadding = 36;
 		private const int SmallPadding = 12;
 
 
@@ -40,63 +41,105 @@ namespace DragonResonance.Editor.Settings
 				_settings = DragonResonanceEditorSettings.instance;
 
 				Rect bannerRect = GUILayoutUtility.GetAspectRect((float)_bannerImage.width / _bannerImage.height);
-				GUIStyle fullSection = new() { padding = new RectOffset(LargePadding, LargePadding, LargePadding, LargePadding) };
-				GUIStyle separatedSection = new() { padding = new RectOffset(LargePadding, LargePadding, SmallPadding, SmallPadding) };
+				GUIStyle fullSection = new() { padding = new RectOffset(MediumPadding, MediumPadding, MediumPadding, MediumPadding) };
+				GUIStyle separatedSection = new() { padding = new RectOffset(MediumPadding, MediumPadding, SmallPadding, SmallPadding) };
+				GUIStyle copyrightSection = new() { padding = new RectOffset(LargePadding, LargePadding, SmallPadding, SmallPadding) };
 
 				EditorGUI.DrawTextureTransparent(bannerRect, _bannerImage);
 
 				EditorGUI.BeginChangeCheck();
 				EditorGUILayout.BeginVertical(fullSection);
 				{
-					EditorGUILayout.LabelField("Links", EditorStyles.whiteLargeLabel);
-					EditorGUILayout.BeginHorizontal(separatedSection);
 					{
-						if (GUILayout.Button("Website"))
-							Application.OpenURL("https://www.dragonresonance.com/");
-						if (GUILayout.Button("GitHub"))
-							Application.OpenURL("https://github.com/dragonresonance");
-						if (GUILayout.Button("GitLab"))
-							Application.OpenURL("https://gitlab.com/dragonresonance");
-					}
-					EditorGUILayout.EndHorizontal();
-
-					GUILayout.Space(LargePadding);
-
-					EditorGUILayout.LabelField("Developers", EditorStyles.whiteLargeLabel);
-					{
-						EditorGUILayout.BeginVertical(separatedSection);
+						EditorGUILayout.LabelField("Links", EditorStyles.whiteLargeLabel);
+						EditorGUILayout.BeginHorizontal(separatedSection);
 						{
-							EditorGUILayout.LabelField("David Tabernero M.", EditorStyles.largeLabel);
-							EditorGUILayout.BeginHorizontal();
+							if (GUILayout.Button("Website"))
+								Application.OpenURL("https://www.dragonresonance.com/");
+							if (GUILayout.Button("GitHub"))
+								Application.OpenURL("https://github.com/dragonresonance");
+							if (GUILayout.Button("GitLab"))
+								Application.OpenURL("https://gitlab.com/dragonresonance");
+						}
+						EditorGUILayout.EndHorizontal();
+					}
+					GUILayout.Space(MediumPadding);
+					{
+						EditorGUILayout.LabelField("Bug reports & Feature requests", EditorStyles.whiteLargeLabel);
+						EditorGUILayout.BeginHorizontal(separatedSection);
+						{
+							if (GUILayout.Button("Submit DevKit issue"))
+								Application.OpenURL("https://github.com/dragonresonance/unity-devkit/issues/new/choose");
+							if (GUILayout.Button("Submit ToolSet issue"))
+								Application.OpenURL("https://github.com/dragonresonance/unity-toolset/issues/new/choose");
+						}
+						EditorGUILayout.EndHorizontal();
+					}
+					GUILayout.Space(MediumPadding);
+					{
+						EditorGUILayout.LabelField("Developers", EditorStyles.whiteLargeLabel);
+						{
+							EditorGUILayout.BeginVertical(separatedSection);
 							{
-								if (GUILayout.Button("Website"))
-									Application.OpenURL("https://tabernero.dev/");
-								if (GUILayout.Button("X"))
-									Application.OpenURL("https://x.com/davidtabernerom");
-								if (GUILayout.Button("GitHub"))
-									Application.OpenURL("https://github.com/davidtabernerom");
-								if (GUILayout.Button("LinkedIn"))
-									Application.OpenURL("https://www.linkedin.com/in/davidtabernerom/");
+								EditorGUILayout.LabelField("David Tabernero M.", EditorStyles.largeLabel);
+								EditorGUILayout.BeginHorizontal();
+								{
+									if (GUILayout.Button("Website"))
+										Application.OpenURL("https://tabernero.dev/");
+									if (GUILayout.Button("X"))
+										Application.OpenURL("https://x.com/davidtabernerom");
+									if (GUILayout.Button("GitHub"))
+										Application.OpenURL("https://github.com/davidtabernerom");
+									if (GUILayout.Button("LinkedIn"))
+										Application.OpenURL("https://www.linkedin.com/in/davidtabernerom/");
+								}
+								EditorGUILayout.EndHorizontal();
+								if (EditorGUILayout.LinkButton("Buy David a Ko-Fi!"))
+									Application.OpenURL("https://ko-fi.com/davidtabernerom");
 							}
-							EditorGUILayout.EndHorizontal();
-							if (EditorGUILayout.LinkButton("Buy David a Ko-Fi!"))
-								Application.OpenURL("https://ko-fi.com/davidtabernerom");
+							EditorGUILayout.EndVertical();
+						}
+					}
+					GUILayout.Space(MediumPadding);
+					{
+						EditorGUILayout.BeginVertical(copyrightSection);
+						{
+							GUICenteredLabel("Copyright © 2021-2026. All rights reserved.");
+							GUICenteredLabel("Licensed under the Apache License, Version 2.0.");
+							GUICenteredLink("See LICENSE.md for more info.", "https://github.com/dragonresonance/unity-toolset/blob/master/LICENSE.md");
 						}
 						EditorGUILayout.EndVertical();
 					}
-
-					GUILayout.Space(LargePadding);
-
-					EditorGUILayout.LabelField("Copyright © 2021-2026. All rights reserved.", EditorStyles.centeredGreyMiniLabel);
-					EditorGUILayout.LabelField("Licensed under the Apache License, Version 2.0.", EditorStyles.centeredGreyMiniLabel);
-					EditorGUILayout.LabelField("See LICENSE.md for more info.", EditorStyles.centeredGreyMiniLabel);
-
 					//_settings.EditorTestBool = EditorGUILayout.Toggle("Editor Test Bool", _settings.EditorTestBool);
 					//_settings.EditorTestString = EditorGUILayout.TextField("Editor Test String", _settings.EditorTestString);
 				}
 				EditorGUILayout.EndVertical();
 				if (EditorGUI.EndChangeCheck())
 					_settings.Save();
+			}
+
+		#endregion
+
+
+		#region Privates
+
+			private static void GUICenteredLabel(string text)
+			{
+				EditorGUILayout.BeginHorizontal();
+				GUILayout.FlexibleSpace();
+				EditorGUILayout.LabelField(text, GUILayout.Width(EditorStyles.label.CalcSize(new GUIContent(text)).x));
+				GUILayout.FlexibleSpace();
+				EditorGUILayout.EndHorizontal();
+			}
+
+			private static void GUICenteredLink(string text, string url)
+			{
+				EditorGUILayout.BeginHorizontal();
+				GUILayout.FlexibleSpace();
+				if (EditorGUILayout.LinkButton(text))
+					Application.OpenURL(url);
+				GUILayout.FlexibleSpace();
+				EditorGUILayout.EndHorizontal();
 			}
 
 		#endregion
