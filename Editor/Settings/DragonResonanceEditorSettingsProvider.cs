948214debc5303f2178e18a0b02eea56daf1a88c
Tabernero <16870364+davidtabernerom@users.noreply.github.com>
2026-04-03 03:13:25 +0200

    Customize welcome settings page inside project settings

--- Editor/Settings/DragonResonanceEditorSettingsProvider.cs
+++ Editor/Settings/DragonResonanceEditorSettingsProvider.cs
@@ -2,6 +2,7 @@
 
 
 using UnityEditor;
+using UnityEngine;
 
 
 namespace DragonResonance.Editor.Settings
@@ -9,34 +10,91 @@ namespace DragonResonance.Editor.Settings
 	public class DragonResonanceEditorSettingsProvider : SettingsProvider
 	{
 		private const string SettingsPath = "Project/Dragon Resonance";
+		private const string BannerGUID = "9a60bf40f7c4aa74d94ea7c32361af13";
+		private const int LargePadding = 36;
+		private const int SmallPadding = 12;
 
 
 		private static DragonResonanceEditorSettings _settings;
+		private static Texture2D _bannerImage = null;
 
 
 		#region Constructors
 
-			public DragonResonanceEditorSettingsProvider(string path, SettingsScope scope) : base(path, scope) { }
+			[SettingsProvider]
+			public static SettingsProvider Create() => new DragonResonanceEditorSettingsProvider(SettingsPath, SettingsScope.Project);
+
+			public DragonResonanceEditorSettingsProvider(string path, SettingsScope scope) : base(path, scope)
+			{
+				string bannerPath = AssetDatabase.GUIDToAssetPath(BannerGUID);
+				_bannerImage = AssetDatabase.LoadAssetAtPath<Texture2D>(bannerPath);
+			}
 
 		#endregion
 
 
 		#region Publics
 
-			[SettingsProvider]
-			public static SettingsProvider Create()
-			{
-				return new DragonResonanceEditorSettingsProvider(SettingsPath, SettingsScope.Project);
-			}
-
 			public override void OnGUI(string searchContext)
 			{
 				_settings = DragonResonanceEditorSettings.instance;
+
+				Rect bannerRect = GUILayoutUtility.GetAspectRect((float)_bannerImage.width / _bannerImage.height);
+				GUIStyle fullSection = new() { padding = new RectOffset(LargePadding, LargePadding, LargePadding, LargePadding) };
+				GUIStyle separatedSection = new() { padding = new RectOffset(LargePadding, LargePadding, SmallPadding, SmallPadding) };
+
+				EditorGUI.DrawTextureTransparent(bannerRect, _bannerImage);
+
 				EditorGUI.BeginChangeCheck();
+				EditorGUILayout.BeginVertical(fullSection);
 				{
-					_settings.EditorTestBool = EditorGUILayout.Toggle("Editor Test Bool", _settings.EditorTestBool);
-					_settings.EditorTestString = EditorGUILayout.TextField("Editor Test String", _settings.EditorTestString);
+					EditorGUILayout.LabelField("Links", EditorStyles.whiteLargeLabel);
+					EditorGUILayout.BeginHorizontal(separatedSection);
+					{
+						if (GUILayout.Button("Website"))
+							Application.OpenURL("https://www.dragonresonance.com/");
+						if (GUILayout.Button("GitHub"))
+							Application.OpenURL("https://github.com/dragonresonance");
+						if (GUILayout.Button("GitLab"))
+							Application.OpenURL("https://gitlab.com/dragonresonance");
+					}
+					EditorGUILayout.EndHorizontal();
+
+					GUILayout.Space(LargePadding);
+
+					EditorGUILayout.LabelField("Developers", EditorStyles.whiteLargeLabel);
+					{
+						EditorGUILayout.BeginVertical(separatedSection);
+						{
+							EditorGUILayout.LabelField("David Tabernero M.", EditorStyles.largeLabel);
+							EditorGUILayout.BeginHorizontal();
+							{
+								if (GUILayout.Button("Website"))
+									Application.OpenURL("https://tabernero.dev/");
+								if (GUILayout.Button("X"))
+									Application.OpenURL("https://x.com/davidtabernerom");
+								if (GUILayout.Button("GitHub"))
+									Application.OpenURL("https://github.com/davidtabernerom");
+								if (GUILayout.Button("LinkedIn"))
+									Application.OpenURL("https://www.linkedin.com/in/davidtabernerom/");
+							}
+							EditorGUILayout.EndHorizontal();
+							if (EditorGUILayout.LinkButton("Buy David a Ko-Fi!"))
+								Application.OpenURL("https://ko-fi.com/davidtabernerom");
+						}
+						EditorGUILayout.EndVertical();
+					}
+
+					GUILayout.Space(LargePadding);
+
+					EditorGUILayout.LabelField("Copyright © 2021-2026. All rights reserved.", EditorStyles.centeredGreyMiniLabel);
+					EditorGUILayout.LabelField("Licensed under the Apache License, Version 2.0.", EditorStyles.centeredGreyMiniLabel);
+					EditorGUILayout.LabelField("See LICENSE.md for more info.", EditorStyles.centeredGreyMiniLabel);
+
+					//_settings.EditorTestBool = EditorGUILayout.Toggle("Editor Test Bool", _settings.EditorTestBool);
+					//_settings.EditorTestString = EditorGUILayout.TextField("Editor Test String", _settings.EditorTestString);
 				}
+				EditorGUILayout.EndVertical();
 				if (EditorGUI.EndChangeCheck())
 					_settings.Save();
 			}
